0a2c32c330f80e037cf1bc8569ee8f856fc33a99
federico <federicoalvarez710@gmail.com>
2019-12-10 17:59:10 -0300

    agrega authLayout

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { doLogin as doLoginAction } from '../redux/actions';
+import doLoginAction from '../redux/actions';
 import Login from '../components/auth/Login';
 
 
 class LoginContainer extends Component {
-    render(){
-        return (
-            // this.props.loading ? <Loader/> :
-            <Login
-                doLogin={this.props.doLogin}
-                loading={this.props.loading }
-            />
-        )
-    }
+  render() {
+    return (
+    // this.props.loading ? <Loader/> :
+      <Login
+        doLogin={this.props.doLogin}
+        loading={this.props.loading}
+      />
+    );
+  }
 }
 
 const mapDispatchToProps = dispatch => ({
-    doLogin: (email, password) => dispatch(doLoginAction(email, password))
-})
+  doLogin: (email, password) => dispatch(doLoginAction(email, password))
+});
   
 const mapStateToProps = state => ({
-    loading: state.loading,
-})
+  loading: state.loading
+});
   
 export default connect(mapStateToProps, mapDispatchToProps)(LoginContainer);
