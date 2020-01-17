9e7bed6ab09b34ab6c58c3aafa240053a7f04287
Gonzalo Torterolo <gonzatorte@gmail.com>
2020-01-17 16:29:47 -0300

    fix validation bug

--- src/Lib/Utils/formValidator.js
+++ src/Lib/Utils/formValidator.js
@@ -15,16 +15,16 @@ class FormValidator {
     this.validations.forEach(rule => {
       // if the field hasn't already been marked invalid by an earlier rule
       if (!validation[rule.field].isInvalid) {
-        // determine the field value, the method to invoke and optional args from 
+        // determine the field value, the method to invoke and optional args from
         // the rule definition
-        if (state[rule.field]) {
+        if (state[rule.field] !== undefined) {
           const field_value = state[rule.field].toString();
           const args = rule.args || [];
-          const validation_method = 
+          const validation_method =
                 typeof rule.method === 'string' ?
-                  validator[rule.method] : 
+                  validator[rule.method] :
                   rule.method;
-                
+
           // call the validation_method with the current field value as the first
           // argument, any additional arguments, and the whole state as a final
           // argument.  If the result doesn't match the rule.validWhen property,
@@ -37,7 +37,7 @@ class FormValidator {
         }
       }
     });
-    
+
     return validation;
   }
 
