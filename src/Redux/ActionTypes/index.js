1096f036b86cac84e63ca77d779810affd58d1db
federico <federicoalvarez710@gmail.com>
2020-02-05 14:20:14 -0300

    fix register user

--- src/Redux/ActionTypes/index.js
+++ src/Redux/ActionTypes/index.js
@@ -1,7 +1,13 @@
 import * as alert from "./alertActionsTypes";
 import * as modal from "./modalActionTypes";
 import * as user from "./userActionsTypes";
+import * as loader from "./loaderActionsTypes";
 
-const combined = { ...alert, ...modal, ...user };
+const combined = { 
+  ...alert, 
+  ...modal, 
+  ...user, 
+  ...loader
+};
 
 export default combined;
